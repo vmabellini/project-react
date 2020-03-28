03211bdfd446c44fd73339bc6c8515c8688aefe4
Vinicius Mabellini <vpmabellini@gmail.com>
2020-03-28 19:37:46 -0300

    Adding react router and links to photos and cart

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from "react";
 import Header from "./components/Header";
 import Cart from "./pages/Cart";
 import Photos from "./pages/Photos";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
       <h1>Home Page</h1>
+
+      <Switch>
+        <route exact path="/">
+          <Photos />
+        </route>
+        <route path="/cart">
+          <Cart />
+        </route>
+      </Switch>
     </div>
   );
 }
