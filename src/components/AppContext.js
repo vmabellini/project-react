5d70c458d5490cacd1fc90966fb316b063a04cf4
Vinicius Mabellini <vpmabellini@gmail.com>
2020-03-29 15:59:29 -0300

    Calling images.json from remote url

--- src/components/AppContext.js
+++ src/components/AppContext.js
@@ -1,9 +1,19 @@
-import React, { useContext, createContext, useState } from "react";
+import React, { useContext, createContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
 function AppContextProvider({ children }) {
   const [pics, setPics] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    )
+      .then(response => response.json())
+      .then(data => {
+        setPics(data);
+      });
+  }, []);
 
   return <AppContext.Provider value={pics}>{{ children }}</AppContext.Provider>;
 }
