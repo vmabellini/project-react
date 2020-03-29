8d2ad1fc8cc643fbbadbec8b55ad1491e3c1bda5
Vinicius Mabellini <vpmabellini@gmail.com>
2020-03-29 17:24:53 -0300

    Favoriting pic

--- src/components/AppContext.js
+++ src/components/AppContext.js
@@ -5,6 +5,20 @@ const AppContext = createContext();
 function AppContextProvider({ children }) {
   const [pics, setPics] = useState([]);
 
+  function toggleFavorite(id) {
+    const updatedArr = pics.map(photo => {
+      if (photo.id === id) {
+        return {
+          ...photo,
+          isFavorite: !photo.isFavorite
+        };
+      }
+      return photo;
+    });
+
+    setPics(updatedArr);
+  }
+
   useEffect(() => {
     fetch(
       "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
@@ -15,7 +29,11 @@ function AppContextProvider({ children }) {
       });
   }, []);
 
-  return <AppContext.Provider value={{ pics }}>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={{ pics, toggleFavorite }}>
+      {children}
+    </AppContext.Provider>
+  );
 }
 
 export { AppContextProvider, AppContext };
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { AppContext } from "../components/AppContext";
 
 function Image({ className, img }) {
   const [hovered, setHovered] = useState(false);
 
-  const heartIcon = hovered && <i className="ri-heart-line favorite"></i>;
+  const { toggleFavorite } = useContext(AppContext);
+
+  const heartIcon = hovered && (
+    <i
+      className="ri-heart-line favorite"
+      onClick={() => toggleFavorite(img.id)}
+    ></i>
+  );
   const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>;
 
   return (
