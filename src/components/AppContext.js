da5606ecc44a47f9eab967c7c5b59033a0c6d674
Vinicius Mabellini <vpmabellini@gmail.com>
2020-04-12 17:51:34 -0300

    Add to cart

--- src/components/AppContext.js
+++ src/components/AppContext.js
@@ -4,13 +4,14 @@ const AppContext = createContext();
 
 function AppContextProvider({ children }) {
   const [pics, setPics] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
   function toggleFavorite(id) {
-    const updatedArr = pics.map(photo => {
+    const updatedArr = pics.map((photo) => {
       if (photo.id === id) {
         return {
           ...photo,
-          isFavorite: !photo.isFavorite
+          isFavorite: !photo.isFavorite,
         };
       }
       return photo;
@@ -19,18 +20,24 @@ function AppContextProvider({ children }) {
     setPics(updatedArr);
   }
 
+  function addToCart(image) {
+    setCartItems((prevItems) => [...prevItems, image]);
+
+    console.log(cartItems);
+  }
+
   useEffect(() => {
     fetch(
       "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
     )
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         setPics(data);
       });
   }, []);
 
   return (
-    <AppContext.Provider value={{ pics, toggleFavorite }}>
+    <AppContext.Provider value={{ pics, toggleFavorite, addToCart }}>
       {children}
     </AppContext.Provider>
   );
